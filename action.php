0e5d26bd5dc88cc055a7ad899f0495b258580d70
HemaKhater <101071912+HemaKhater@users.noreply.github.com>
2022-03-08 10:41:29 -0800

    Add files via upload

---
+++ action.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "welcome name is". " " .$_POST['name']."<br>";
+echo "your email is" . " " .$_POST['email'] ."<br>";
+echo "password". " ".$_POST['password']."<br>";
+echo "your address is" . " " .$_POST['address'] ."<br>";
+echo "your url is" . " " .$_POST['linkedin'];
