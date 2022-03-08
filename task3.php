defc31b56b96e6cb9733213e1a2f16885d667c7d
HemaKhater <101071912+HemaKhater@users.noreply.github.com>
2022-03-08 10:41:07 -0800

    Add files via upload

---
+++ task3.php
@@ -0,0 +1,31 @@
+<?php
+?>
+
+<form method="post"action="action.php">
+name:
+	<input type="text" name="name" value="user name"  required> <br>
+email:
+    <input type="text" name="email" value="your email" required> <br>
+password:   
+	<input type="password" name="password" value="password" minlengh=6 required> <br>
+address:
+    <input type="text" name="address" value="your addresss" lengh=10 required> <br>
+linkedin url:
+    <input type="url" name="linkedin" value="enter url"> <br>
+
+
+	<input type="submit" value="login">
+	
+
+
+
+
+
+
+
+
+
+
+
+
+</form>
