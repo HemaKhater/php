1d12cdfe96d2fa37d62e8ea1a7f18ffeb94b0520
HemaKhater <101071912+HemaKhater@users.noreply.github.com>
2022-03-09 17:21:02 -0800

    Delete action.php

--- action.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "welcome name is". " " .$_POST['name']."<br>";
-echo "your email is" . " " .$_POST['email'] ."<br>";
-echo "password". " ".$_POST['password']."<br>";
-echo "your address is" . " " .$_POST['address'] ."<br>";
-echo "your url is" . " " .$_POST['linkedin'];
